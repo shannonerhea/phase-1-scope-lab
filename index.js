4950bcc916c33583dacce747930ace0e5b3563cd
shannone <shannone426@gmail.com>
2022-08-10 09:48:32 -0400

    810

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-//customerName: Declare a variable in global scope 
-//called customerName using the var keyword and assign it the value 'bob'.
-var customerName = "bob";
+// //customerName: Declare a variable in global scope 
+// //called customerName using the var keyword and assign it the value 'bob'.
+// var customerName = "bob";
 
 
-//upperCaseCustomerName(): Write a function that accesses that global 
-//customerName variable, and uppercases it.
-// use toUpperCase()
-function upperCaseCustomerName() {
-    return customerName.toUpperCase();
-};
+// //upperCaseCustomerName(): Write a function that accesses that global 
+// //customerName variable, and uppercases it.
+// // use toUpperCase()
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// };
 
 
-/**setBestCustomer(): Write a function that when called, 
- * declares a variable called bestCustomer in global scope 
- * and assigns it to be 'not bob'. (Poor Bob.) Also, poor us, 
- * declaring a global variable from inside a function is one of 
- * those things we would never want to do normally, but is good for 
- * us to explore right now. */
+// /**setBestCustomer(): Write a function that when called, 
+//  * declares a variable called bestCustomer in global scope 
+//  * and assigns it to be 'not bob'. (Poor Bob.) Also, poor us, 
+//  * declaring a global variable from inside a function is one of 
+//  * those things we would never want to do normally, but is good for 
+//  * us to explore right now. */
+//  function setBestCustomer() {
+//     bestCustomer = "not bob";
+//  };
+
+// //overwriteBestCustomer(): See the consequences of declaring 
+// //a variable in global scope, by writing a new
+// //function called overwriteBestCustomer() 
+// //that changes that bestCustomer variable to 'maybe bob'.
+// function overwriteBestCustomer() {
+//     bestCustomer = "maybe bob";
+// };
+
+// /**leastFavoriteCustomer and changeLeastFavoriteCustomer(): 
+//  * Now declare a constant (using the const keyword) 
+//  * in global scope called leastFavoriteCustomer; 
+//  * be sure to assign it some initial value. */
 
 
-//overwriteBestCustomer(): See the consequences of declaring 
-//a variable in global scope, by writing a new
-//function called overwriteBestCustomer() 
-//that changes that bestCustomer variable to 'maybe bob'.
+// /**write a function called changeLeastFavoriteCustomer() 
+//  * that attempts to change that constant 
+//  * - notice what JavaScript does when you try to change the constant. */
+// function changeLeastFavoriteCustomer() {};
 
-
-/**leastFavoriteCustomer and changeLeastFavoriteCustomer(): 
- * Now declare a constant (using the const keyword) 
- * in global scope called leastFavoriteCustomer; 
- * be sure to assign it some initial value. */
-
-
-/**write a function called changeLeastFavoriteCustomer() 
- * that attempts to change that constant 
- * - notice what JavaScript does when you try to change the constant. */
